dd26d54d8fd12463203e37a1adce3f20e67c5145
Vaibhav <rushukla7@gmail.com>
2024-06-28 01:00:53 +0530

    Initial commit

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Editor from './pages/Editor.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/editor",
+        element:<Editor/>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
---
+++ Frontend/src/pages/Editor.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Editor() {
+  return (
+      <>
+        <div className='flex flex-wrap'>
+          This is my editor
+        </div>
+      </>
+  )
+}
+
+export default Editor
